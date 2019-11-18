373b9e8f83f07e29500930fb48de0b6945ceaa6d
Methaxyz <56343347+Methaxyz@users.noreply.github.com>
2019-11-18 11:49:30 +0700

    18/11

---
+++ cal borrow.py
@@ -0,0 +1,27 @@
+"""borrow"""
+def borrow():
+    """find the borrow cost function"""
+    print("กรุณากรอกข้อมูลตามลำดับ")
+    print("1.รายได้หรือเงินเดือน")
+    print("2.จำนวนผ่อนชำระหนี้สูงสุดที่แบกรับได้ต่อเงินเดือน")
+    print("3.อัตราส่วน ของเงินผ่อนชำระต่องวด")
+    print("4.หนี้สินปัจจุบันที่ต้องชำระต่อเดือน")
+    
+    money = int(input())
+    max_money = int(input())
+    print("กรุณากรอกข้อมูลตามลำดับ")
+    print("1.เศษ")
+    print("2.ส่วน")
+    func_top = int(input())
+    func_low = int(input())
+    dept = int(input())
+    calfunc = func_top/func_low
+    cal_per = max_money/money
+    cal_dept = max_money-dept
+    cal_bow = ((money*cal_per)/max_money)-dept
+    total = (cal_bow*func_low)/func_top
+    print("ความสามารถในการกู้สูงสุดของคุณคือ %.2f"%total)
+borrow()
+
+        
+    
