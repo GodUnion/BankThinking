f0f661dfb6428a67791819417d31d46cac095b9d
polwiriya <56337163+polwiriya@users.noreply.github.com>
2019-12-15 17:11:54 +0700

    Update installment.py

--- installment.py
+++ installment.py
@@ -1,15 +1,14 @@
 """installment"""
 def installment():
-	"""installment"""
+	"""this fuction cal installment"""
 	print("กรุณากรอกข้อมูลตามลำดับ")
 	print("1.จำนวนเงินที่ต้องการผ่อน")
-	print("2.อัตราดอกเบี้ย")
+	print("2.อัตราดอกเบี้ย เป็น%")
 	print("3.ระยะเวลาในการผ่อน")
 	monney = float(input())
 	interest = float(input())
 	time = int(input())
 	increase = (monney+(monney*(interest/100)))//time
 	ans = round(increase, 0)
-	
-	print(increase)
+	print("ท่านจะต้องผ่อนเดือนละ %.2f บาท"%increase)
 installment()
